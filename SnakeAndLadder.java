076cb4f4d9ae504c103f8d6e57d271d6e859b6dd
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-26 08:52:29 +0530

    added play option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,23 @@ public class SnakeAndLadder {
         System.out.println("Start position is :" +startPosition);
         System.out.println("Die roll is :" + dieRoll);
 
+        int option = (int) (((Math.random()*10)%3)+1);
+
+        System.out.println("Option play is : " +option);
+
+        if (option == 1 ) {
+            System.out.println("Player cant move");
+        } else if (option == 2) {
+            System.out.println("Player got ladder");
+            startPosition=startPosition+dieRoll;
+            System.out.println("player moves " +dieRoll+ " steps forward");
+
+        } else {
+            System.out.println("Player got snake");
+            startPosition=startPosition-dieRoll;
+            System.out.println("player moves " +dieRoll+ " steps backward");
+        }
+
     }
 
 }
