5f5744a15da0bf95a0f16b7367ceaa66a703e7cf
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-26 08:39:12 +0530

    added die roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,11 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int startPosition=0;
+        int dieRoll = (int) (((Math.random()*10)%6)+1);
+
+        System.out.println("Start position is :" +startPosition);
+        System.out.println("Die roll is :" + dieRoll);
+
     }
 
 }
